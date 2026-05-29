cd9c3d48d73101273e8f05dd653422a1f03f912b
MelyGM <133719381+MelyGM@users.noreply.github.com>
2026-05-29 16:00:20 -0700

    Implementacion del constructor  y funcion auxiliar

--- crucigramas.py
+++ crucigramas.py
@@ -1,10 +1,11 @@
 import csps
 import time
 
-
+"""
 class Crucigrama(csps.ProblemaCSP):
     def __init__(self, pos_ini):
-        self.X = # TODO: definir el conjunto de variables
+
+        self.X = #
         self.D = # TODO: definir el dominio de cada variable
         self.N = # TODO: definir el conjunto de vecinos de cada variable
         
@@ -14,13 +15,20 @@ class Crucigrama(csps.ProblemaCSP):
         pass
     
 def prueba_crucigrama(verticales, horizontales, consistencia=1):
-    
-    # TODO: Probar el CSP del crucigrama con el grafo de restricciones con consistencia dada y medir el tiempo que tarda en resolverlo. Imprimir la asignación resultante, el número de backtrackings realizados y el tiempo que tardó en resolverlo.
-    
-    raise NotImplementedError("Completa la función prueba_crucigrama para probar tu implementación del CSP del crucigrama")
+    pos_ini = {
+        "verticales": verticales,
+        "horizontales": horizontales
+    }
+    programa = Crucigrama(pos_ini)
+    t0 = time.time()
+    asig =csps.asignacion_completa(problema, consistencia=consistencia)
+    t_lapso=time.time() - t0
+
+    print(asig)
+    print("Backtracking con consistencia {}: {} segundos".format(consistencia, t_lapso))
 
 if __name__ == "__main__":
     
     prueba_crucigrama(...) # TODO: definir los crucigramas a probar
 
-         
+   """"      
