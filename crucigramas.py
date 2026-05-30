d85c091bb811b5e0b2fbdb9b02dc7de50d09fb6e
MelyGM <133719381+MelyGM@users.noreply.github.com>
2026-05-29 17:25:20 -0700

    Implementacion de restricciones

--- crucigramas.py
+++ crucigramas.py
@@ -1,27 +1,103 @@
 import csps
 import time
 
-"""
 class Crucigrama(csps.ProblemaCSP):
     def __init__(self, pos_ini):
 
-        self.X = #
-        self.D = # TODO: definir el dominio de cada variable
-        self.N = # TODO: definir el conjunto de vecinos de cada variable
-        
+        verticales = pos_ini["verticales"]
+        horizontales = pos_ini["horizontales"]
+        n = pos_ini["filas"]
+        m = pos_ini["columnas"]
 
-    def restriccion_binaria(self, xi, vi, xj, vj):
-        # TODO: definir la función de restricción binaria entre las variables xi y xj
-        pass
+        self.X = set()
+
+        for w in horizontales:
+            self.X.add((w,"H"))
+        for w in verticales:
+            self.X.add((w,"V"))
+
+        self.D={}
+
+        for var in self.X:
+            w, direccion=var
+            L=len(w)
+            dominio = set()
+
+            if direccion == "H":
+                if L <=m:
+                    dominio ={
+                        (r,c)
+                        for r in range(n)
+                        for c in range(m-L+1)
+                    }
+
+            elif direccion == "V":
+                if L <= n:
+                    dominio ={
+                        (r,c)
+                        for r in range(n-L+1)
+                        for c in range(m)
+                    }
+
+            self.D[var] = dominio
+
+        self.N={
+            x: self.X.difference({x})
+            for x in self.X
+        }
+
+    def casillas_ocupadas(self, palabra, direccion, fila, columna):
+        casillas =[]
+        for i in range(len(palabra)):
+            if direccion =="H":
+                casillas.append((fila, columna + i, palabra[i]))
+            else:
+                casillas.append((fila + i, columna, palabra[i]))
+        return casillas
     
-def prueba_crucigrama(verticales, horizontales, consistencia=1):
+    def restriccion_binaria(self, xi, vi, xj, vj):
+        palabra1, dir1 = xi
+        palabra2, dir2 = xj
+        fila1,col1 = vi
+        fila2,col2 = vj
+        casillas1 = {}
+        casillas2 = {}
+
+        for i, letra in enumerate(palabra1):
+            if dir1 == "H":
+                casillas1[(fila1, col1 + i)] = letra
+            else:
+                casillas1[(fila1 + i, col1)] = letra
+
+        for i, letra in enumerate(palabra2):
+            if dir2 == "H":
+                casillas2[(fila2, col2 + i)] = letra
+            else:
+                casillas2[(fila2 + i, col2)] = letra
+
+        comunes = set(casillas1.keys()) & set(casillas2.keys())
+        
+        for pos in comunes:
+            if casillas1[pos] != casillas2[pos]:
+                return False
+        return True
+
+def prueba_crucigrama(consistencia=1):
+    with open("vertical.txt", "r", encoding="utf-8") as f:
+        verticales = [line.strip() for line in f if line.strip()]
+
+    with open("horizontal.txt", "r", encoding="utf-8") as f:
+        horizontales = [line.strip() for line in f if line.strip()]
+
     pos_ini = {
         "verticales": verticales,
-        "horizontales": horizontales
+        "horizontales": horizontales,
+        "filas": 15,
+        "columnas":15
     }
     programa = Crucigrama(pos_ini)
     t0 = time.time()
-    asig =csps.asignacion_completa(problema, consistencia=consistencia)
+    asig =csps.asignacion_completa(programa, consistencia=consistencia)
     t_lapso=time.time() - t0
 
     print(asig)
@@ -29,6 +105,4 @@ def prueba_crucigrama(verticales, horizontales, consistencia=1):
 
 if __name__ == "__main__":
     
-    prueba_crucigrama(...) # TODO: definir los crucigramas a probar
-
-   """"      
+    prueba_crucigrama(consistencia=1) 
